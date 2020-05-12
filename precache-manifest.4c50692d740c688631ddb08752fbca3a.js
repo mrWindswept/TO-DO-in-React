d2c3f300ce72647ed266f6a0ac9c102c72640b43
Kunal Patil <kunallanuk9@gmail.com>
2020-05-13 01:33:38 +0530

    Updates

---
+++ precache-manifest.4c50692d740c688631ddb08752fbca3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54f5918c359c2f46d44ec20b5264bb2",
+    "url": "/TO-DO-in-React/index.html"
+  },
+  {
+    "revision": "0183abf36c9feb53ddb3",
+    "url": "/TO-DO-in-React/static/css/main.8d104901.chunk.css"
+  },
+  {
+    "revision": "bd6c29934f0290d451b9",
+    "url": "/TO-DO-in-React/static/js/2.93777268.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TO-DO-in-React/static/js/2.93777268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0183abf36c9feb53ddb3",
+    "url": "/TO-DO-in-React/static/js/main.004675c4.chunk.js"
+  },
+  {
+    "revision": "e7de0d10ac851aa3dc31",
+    "url": "/TO-DO-in-React/static/js/runtime-main.24c4facf.js"
+  }
+]);
